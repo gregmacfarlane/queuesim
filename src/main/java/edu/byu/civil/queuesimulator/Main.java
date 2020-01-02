07e1d56b0a9380ca0c71fc07f549035f1d53d10e
Greg Macfarlane <gregmacfarlane@gmail.com>
2020-01-02 10:17:31 -0700

    Extend to two random distributions

--- src/main/java/edu/byu/civil/queuesimulator/Main.java
+++ src/main/java/edu/byu/civil/queuesimulator/Main.java
@@ -10,9 +10,13 @@ public class Main {
     // When we run the class, this gets executed.
     public static void main(String[] args) {
 
-        // Create a new random arrival distribution
-        PoissonArrival pa1 = new PoissonArrival(1, 1);
-        log.info("The next random number is: " + pa1.nextRandom());
+        // Create two new random arrival distribution
+        PoissonArrival pa1 = new PoissonArrival(0.5, 1);
+        PoissonArrival pa2 = new PoissonArrival(0.1, 1);
+        for(int i = 0; i < 10; i++) {
+            log.info("The " + i + "th random number is: " + pa1.nextRandom());
+            log.info("The " + i + "th random number is: " + pa2.nextRandom());
+        }
 
     }
 }
