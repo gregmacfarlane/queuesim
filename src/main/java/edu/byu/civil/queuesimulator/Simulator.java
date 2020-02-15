421d3908bd7f751957e8c2be52a74abbbb74c1de
Greg Macfarlane <gregmacfarlane@byu.edu>
2020-02-15 15:44:35 -0700

    Provide framework for simulator

--- src/main/java/edu/byu/civil/queuesimulator/Simulator.java
+++ src/main/java/edu/byu/civil/queuesimulator/Simulator.java
@@ -6,10 +6,30 @@ public class Simulator {
     private static Logger log = Logger.getLogger(Simulator.class);
 
 
+    /**
+     * Instantiate an object of the simulator class with a given M/M/N (Inf, FIFO)
+     * @param lambda Arrival rate
+     * @param mu Service rate
+     * @param N Number of service counters
+     */
+    public Simulator(double lambda, double mu, int N){
+
+    }
+
+    /**
+     * Run the simulation
+     */
+    public void runSimulation() {
+
+    }
+
+
     // When we run the class, this gets executed.
     public static void main(String[] args) {
-        log.info("This is information");
-
+        log.info("Beginning a new BYU Queuesim");
+        Simulator simulator = new Simulator(5, 10, 1);
+        simulator.runSimulation();
+        log.info("Ending BYU Queuesim");
 
     }
 }
