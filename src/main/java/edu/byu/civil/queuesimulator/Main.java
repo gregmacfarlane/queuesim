d145206b0e30cfa6b3cd02497fad79b2e94e2fbf
Greg Macfarlane <gregmacfarlane@byu.edu>
2020-02-15 15:36:21 -0700

    Reset for class instruction

--- src/main/java/edu/byu/civil/queuesimulator/Main.java
+++ src/main/java/edu/byu/civil/queuesimulator/Main.java
@@ -5,18 +5,11 @@ import org.apache.log4j.Logger;
 public class Main {
     private static Logger log = Logger.getLogger(Main.class);
 
-    private static String myString = "Hello World";
 
     // When we run the class, this gets executed.
     public static void main(String[] args) {
+        log.info("This is information");
 
-        // Create two new random arrival distribution
-        PoissonArrival pa1 = new PoissonArrival(0.5, 1);
-        PoissonArrival pa2 = new PoissonArrival(0.1, 1);
-        for(int i = 0; i < 10; i++) {
-            log.info("The " + i + "th random number is: " + pa1.nextRandom());
-            log.info("The " + i + "th random number is: " + pa2.nextRandom());
-        }
 
     }
 }
