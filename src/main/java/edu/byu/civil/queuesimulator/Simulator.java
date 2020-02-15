8dbedfe6092a2c01effc60267a538e6438b10313
Greg Macfarlane <gregmacfarlane@byu.edu>
2020-02-15 15:38:44 -0700

    Rename main to simulator

--- src/main/java/edu/byu/civil/queuesimulator/Simulator.java
+++ src/main/java/edu/byu/civil/queuesimulator/Simulator.java
@@ -2,8 +2,8 @@ package edu.byu.civil.queuesimulator;
 
 import org.apache.log4j.Logger;
 
-public class Main {
-    private static Logger log = Logger.getLogger(Main.class);
+public class Simulator {
+    private static Logger log = Logger.getLogger(Simulator.class);
 
 
     // When we run the class, this gets executed.
